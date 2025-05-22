17b07658f6f0b4bfdf9113f1ba5785bd364511ac
Elian <ebenois@ensc.fr>
2025-05-22 13:13:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="../image/logoc.png">
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&family=Inter:wght@400;600&display=swap" rel="stylesheet">
+        <title>Accueil | COG'AIR SHOW</title>
+    </head>
+
+    <body class="bg-black">
+        <div class="bg-black">
+            <div class=" pl-5 sm:pl-10 md:pl-15 lg:pl-20 xl:pl-25 py-1 sm:py-1 md:py-2 lg:py-3 xl:py-4">
+                <a class="flex items-center w-fit" href="../index.php">
+                    <img src="../image/logon.png"  class="h-4 sm:h-8 md:h-10 lg:h-14 xl:h-18" alt="logo">
+                    <img src="../image/titre.png"  class="pl-2 h-4 sm:h-5 md:h-6 lg:h-7 xl:h-8" alt="nom_du_projet">  
+                </a>
+            </div>
+        </div>
+        <div class="font-mono bg-zinc-900 text-xs sm:text-base md:text-lg lg:text-xl xl:lg:text-2xl flex justify-evenly py-1 sm:py-1 md:py-2 lg:py-3 xl:py-4">
+            <a class="hover:text-zinc-400 text-white" href="../page/presentation.html"><b>PRÉSENTATION</b></a>
+            <a class="hover:text-zinc-400 text-white" href="../page/equipe.html"><b>ÉQUIPE</b></a>
+            <a class="hover:text-zinc-400 text-white" href="../page/deroulement.html"><b>DÉROULEMENT</b></a>
+            <a class="hover:text-zinc-400 text-white" href="../fichier/manuel/Manuel MIRAGE 2000 C.pdf"><b>MANUEL</b></a>
+            <a class="hover:text-zinc-400 text-white" href="../page/video.html"><b>VIDEOS</b></a>
+            <a class="hover:text-zinc-400 text-white" href="../page/bilan.html"><b>BILAN</b></a>
+            <a class="hover:text-zinc-400 text-white" href="../page/planVert.html"><b>PLAN VERT</b></a>
+        </div>
+
+        <div class="relative w-full">
+            <img class="w-full h-auto" src="../image/avion.png">
+            <div class="absolute inset-0 flex items-center justify-center">
+                <div class="text-center">
+                    <h1 class="font-mono uppercase sm:text-5xl md:text-6xl lg:text-7xl xl:lg:text-8xl text-nowrap text-white"><b>Piloter le Mirage 2000 C</b></h1>
+                    <p class="font-mono uppercase sm:text-sm md:text-lg lg:text-xl xl:lg:text-3xl text-nowrap text-white">Un projet transdisciplinaire pour apprendre à piloter le Mirage 2000 C.</p>
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            <div class="bg-black ">
+                <div class=" flex items-center justify-end pr-5 sm:pr-10 md:pr-15 lg:pr-20 xl:pr-25">
+                    <img src="../image/logon.png"  class="h-4 sm:h-8 md:h-10 lg:h-14 xl:h-18" alt="logo">
+                    <img src="../image/titre.png"  class="pl-2 h-4 sm:h-5 md:h-6 lg:h-7 xl:h-8" alt="nom_du_projet">  
+                </div>
+                <p class="font-mono text-xs sm:text-sm md:text-bse lg:text-lg xl:lg:text-xl text-white text-right pr-5 sm:pr-10 md:pr-15 lg:pr-20 xl:pr-25 ">© 2025 COG'AIR SHOW – Tous droits réservés</p>
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    </body>
+</html>
